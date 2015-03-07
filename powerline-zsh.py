0e0a36ca96b1e909acb2b864cd98f60788b3ab4a
Andu <andua@channelgrabber.com>
2015-03-07 23:41:19 +0000

    Show git segment in a different color if in detached state

--- powerline-zsh.py
+++ powerline-zsh.py
@@ -30,6 +30,8 @@ class Color:
     REPO_CLEAN_FG = 0  # black
     REPO_DIRTY_BG = 161  # pink/red
     REPO_DIRTY_FG = 15  # white
+    REPO_DETACHED_BG = 149 # a pale yellow
+    REPO_DIRTY_FG = 0 # white
 
     CMD_PASSED_BG = 236
     CMD_PASSED_FG = 15
@@ -217,6 +219,10 @@ def add_git_segment(powerline, cwd):
 
     if has_pending_commits:
         bg = Color.REPO_DIRTY_BG
+        fg = Color.REPO_DIRTY_FG
+
+    if detached:
+        bg = Color.REPO_DETACHED_BG
         fg = Color.REPO_DIRTY_FG
 
     powerline.append(Segment(powerline, ' %s ' % branch, fg, bg))
